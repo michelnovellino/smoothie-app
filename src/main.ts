1da506914c51f5f54198075657925178f065239e
michel novellino <michelnovellino.programador@gmail.com>
2020-05-16 17:37:28 -0400

    fix ts warnings

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import EvaIcons from 'vue-eva-icons';
-
+import EvaIcons from "vue-eva-icons";
 
 Vue.config.productionTip = false;
-Vue.use(EvaIcons)
+Vue.use(EvaIcons);
 
 new Vue({
   router,
